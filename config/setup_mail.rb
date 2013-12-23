648d4aa82eb0505d2b6db0d4ded4d80f362df602
Mickey <sanchez.mickey@gmail.com>
2013-12-23 13:19:19 -0500

    mandrill

---
+++ config/setup_mail.rb
@@ -0,0 +1,6 @@
+ActionMailer::Base.smtp_settings = {
+  :address     => "smtp.mandrillapp.com",
+  :port        => "587",
+  :user_name   => "app18973717@heroku.com",
+  :password    => "Or4njRU39MbZfdSYZMuxww",
+}
