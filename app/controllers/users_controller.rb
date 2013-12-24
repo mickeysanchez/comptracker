2d47fc5cc77a48993eaf04cee9add09ea24c8898
Mickey <sanchez.mickey@gmail.com>
2013-12-23 21:27:39 -0500

    trying to figure out ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,92 +18,65 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @accounts = @user.accounts
     
-    if @accounts.count > 0
-      
-      # set up and initialize Phantomjs Browswer using Watir Webdriver
-
-
-      
-      # scrape Total Rewards if the user has entered a Total Rewards Account
-      if @accounts.find_by(:type_of_account => "Total Rewards")
-        
-        # set up and initialize Watir and Phantomjs in a way that works for the Total Rewards Page.
-        capabilities = Selenium::WebDriver::Remote::Capabilities.phantomjs("phantomjs.page.settings.userAgent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
-        driver = Selenium::WebDriver.for :phantomjs, :desired_capabilities => capabilities
-        switches = ['--ignore-ssl-errors=yes']
-        browser = ::Watir::Browser.new driver, 
-   
-        
-        total_rewards_account = @accounts.find_by(:type_of_account => "Total Rewards")
+    # if @accounts.count > 0
+    # 
+    #   # scrape Total Rewards if the user has entered a Total Rewards Account
+    #   if @accounts.find_by(:type_of_account => "Total Rewards")
+    #     get_total_rewards_comps
+    #   end
+    #   
+    #   # scrape Borgata Rewards
+    #   if @accounts.find_by(:type_of_account => "Borgata Rewards")
+    #     get_borgata_comps
+    #   end
+    # end
     
-        # get to offers page
-        browser.goto  "http://www.totalrewards.com/e-totalrewards/?"
-        browser.input(:id => "username").to_subtype.set(total_rewards_account.username)
-        browser.input(:id => "pin").to_subtype.set(total_rewards_account.password_digest)
-        browser.button(:value => "Sign In").click
-        browser.link(:href => "/TotalRewards/Offers.do?", :text => "Your Offers").click
-        
-        # scrape offers
-        offers = browser.frame(:id => "offerDisplayMod_iframe").divs(:class => "pItem")
-        @total_rewards_comps = {}
-        offers.each do |offer|
-          if offer.span(:class => "lblSubjectOld").exists?
-            name = offer.span(:class => "lblSubjectOld").text
-          else
-            name = offer.span(:class => "lblSub").text
-          end
-          date = offer.div(:class => "expwidth").text
-          
-          @total_rewards_comps[name] = date
-        end
-        
-        @total_rewards_comps.each do |key, value|
-          value.chomp!
-          expiration = value[-10..-1]
-          expiration = DateTime.strptime(expiration, '%m/%d/%Y')
-          today = Date.today
-          @total_rewards_comps[key] = expiration.mjd - today.mjd
-        end
-        
-        browser.close
+  end
+
+  def get_total_rewards_comps
+    # set up and initialize Watir and Phantomjs in a way that works for the Total Rewards Page.
+    capabilities = Selenium::WebDriver::Remote::Capabilities.phantomjs("phantomjs.page.settings.userAgent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
+    driver = Selenium::WebDriver.for :phantomjs, :desired_capabilities => capabilities
+    switches = ['--ignore-ssl-errors=yes']
+    browser = ::Watir::Browser.new driver, 
+
+    
+    total_rewards_account = @accounts.find_by(:type_of_account => "Total Rewards")
+
+    # get to offers page
+    browser.goto  "http://www.totalrewards.com/e-totalrewards/?"
+    browser.input(:id => "username").to_subtype.set(total_rewards_account.username)
+    browser.input(:id => "pin").to_subtype.set(total_rewards_account.password_digest)
+    browser.button(:value => "Sign In").click
+    browser.link(:href => "/TotalRewards/Offers.do?", :text => "Your Offers").click
+    
+    # scrape offers
+    offers = browser.frame(:id => "offerDisplayMod_iframe").divs(:class => "pItem")
+    @total_rewards_comps = {}
+    offers.each do |offer|
+      if offer.span(:class => "lblSubjectOld").exists?
+        name = offer.span(:class => "lblSubjectOld").text
+      else
+        name = offer.span(:class => "lblSub").text
       end
+      date = offer.div(:class => "expwidth").text
       
-      # scrape Borgata Rewards
-      if @accounts.find_by(:type_of_account => "Borgata Rewards")
-        
-        # set up and initialize Watir and Phantomjs in a way that works for the Borgata page.
-        switches = ['--ignore-ssl-errors=yes']
-        browser = Watir::Browser.new :phantomjs, :args => switches
-        
-        # get to offers page
-        borgata_account = @accounts.find_by(:type_of_account => "Borgata Rewards")
-        browser.goto "https://www.theborgata.com/casino/mbr"
-        browser.input(:id => "borgataMainContentWrapped_content_0_ctlLoginForm_txtEmailAddressOrAccountNumber").to_subtype.set(borgata_account.username)
-        browser.input(:id => "borgataMainContentWrapped_content_0_ctlLoginForm_txtPassword").to_subtype.set(borgata_account.password_digest)
-        browser.link(:id => "borgataMainContentWrapped_content_0_ctlLoginForm_btnLogin").click
-        
-        # scrape comps
-        comps = browser.table(:class => "data-list-table offers").tbody.trs
-        @borgata_comps = {}
-        
-        comps.each do |comp|
-          name = comp.td(:class => "offer-name").text
-          date = comp.td(:class => "offer-date").text
-          
-          date.chomp!
-          expiration = date[-8..-1]
-          expiration.lstrip!
-          expiration = Date.strptime(expiration, '%m.%d.%y')
-          
-          today = Date.today
-          
-          @borgata_comps[name] = expiration.mjd - today.mjd
-        end
-      
-        browser.close 
-      end
+      @total_rewards_comps[name] = date
     end
     
+    @total_rewards_comps.each do |key, value|
+      value.chomp!
+      expiration = value[-10..-1]
+      expiration = DateTime.strptime(expiration, '%m/%d/%Y')
+      today = Date.today
+      @total_rewards_comps[key] = expiration.mjd - today.mjd
+    end
+    
+    browser.close
+    
+    respond_to do |format|
+      format.js
+    end
   end
 
   # GET /users/new
@@ -123,8 +96,6 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         sign_in @user
-        
-        UserMailer.welcome_email(@user).deliver
         
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
@@ -156,15 +127,14 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted."
     redirect_to root_url
   end
+  
 
   private
-
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
-    
     
     def correct_user
       @user = User.find(params[:id])
